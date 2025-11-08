d8ba37039e5f2c54339976feb73617877ebfeb2e
Vinitha2206 <vinithauthirapathy@gmail.com>
2025-11-08 01:28:09 -0500

    Add files via upload

---
+++ best food.py
@@ -0,0 +1,9 @@
+food = input("what's the best food?:")
+
+if food == "rise":
+    print("yes, rise is the best.")
+
+elif food == "apple":
+    print("apples arent't my cup of rise.")
+else:
+    print("Never heard of it!")
